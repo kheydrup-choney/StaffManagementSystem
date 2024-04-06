c450a9451599f10211b8be694b3b27ae511bf92e
KheydrupChoney <kheydrup2000204@gmail.com>
2024-04-07 04:24:47 +0600

    started with the basic view of the dashboard of the director

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,30 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
+  <div class="d-flex justify-content-center border" style="height: 100vh;">
+    <div class="col-3 text-center" style="background-color: #6089da;">
+      <div class="m-5">
+        <img src="https://shorter.me/905p0" alt="Bhutan National Medical Service logo" style="width: 7rem;">
+        <h6 class="text-white pt-5">National Medical Service</h6>
+        <h6 class="text-white">SMS:Staff Monitoring System</h6>
+      </div>
+      <nav class="d-flex row">
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <div class="d-flex justify-content-center">
+          <div class="bg-white" style="height: 5rem; width:20rem; border-radius: 1rem;">
+            <img src="" alt="active icon">
+          </div>
+        </div>
       </nav>
     </div>
-  </header>
 
-  <RouterView />
+    <div class="col-9">
+      <RouterView />
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+</script>
